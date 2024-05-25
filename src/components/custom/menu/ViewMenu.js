4960e74a32e133c3d3b7b6130f10b05ff9951d95
KeniBeck <deyler_mena69222@elpoli.edu.co>
2024-05-24 22:58:25 -0500

    api boxCut

--- src/components/custom/menu/ViewMenu.js
+++ src/components/custom/menu/ViewMenu.js
@@ -46,6 +46,21 @@ const ViewMenu = () => {
             router.push('/typeDraw')
     }
 
+    const handleboxCut = async (userData) => {
+        const response = await fetch('/api/boxCut', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userData)
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+
+
+    }
 
     return (
         <div>
@@ -82,7 +97,9 @@ const ViewMenu = () => {
                             </button>
                         </div>
                         <div className="relative">
-                            <button className="w-full rounded-lg bg-red-700 text-white h-9 relative" >
+                            <button
+                                onClick={() => handleboxCut(userData)}
+                                className="w-full rounded-lg bg-red-700 text-white h-9 relative" >
                                 Corte de caja
                                 <FaCashRegister className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4" />
                             </button>
