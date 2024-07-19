3c8a04331682851765d5d8467b25f91880194172
KeniBeck <deyler_mena69222@elpoli.edu.co>
2024-07-19 18:03:22 -0500

    agregando peticion put para fechas

--- src/app/api/model/ticketBuy.js
+++ src/app/api/model/ticketBuy.js
@@ -54,3 +54,29 @@ export const selectLotterySerie = async () => {
 
     return response;
 }
+export const selectLotteryEspecial = async () => {
+    let result = false;
+    let error = false;
+
+    try {
+        let sql = `SELECT * FROM sorteo WHERE Tipo_sorteo ='especial' AND Estatus = 'abierto'`;
+        let [rows] = await pool.query(sql);
+        result = rows;
+
+
+    } catch (err) {
+        error = {
+            "sql": sql,
+            "description": err
+        }
+        console.log(error);
+    }
+
+    let response = {
+        "preocess": 'select table',
+        "status": true,
+        "result": result
+    }
+
+    return response;
+}
