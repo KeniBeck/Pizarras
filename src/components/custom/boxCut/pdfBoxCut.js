eddf22008eaed33c0d1f28592119817daa28faec
KeniBeck <deyler_mena69222@elpoli.edu.co>
2024-09-09 23:11:39 -0500

    espacios en ticket de caja

--- src/components/custom/boxCut/pdfBoxCut.js
+++ src/components/custom/boxCut/pdfBoxCut.js
@@ -84,11 +84,11 @@ const generatePDFBoxCut = async (data) => {
         deuda = data.boletosNormales[0].deuda;
     }
     let caja = totalVentas - totalVentas * 0.10;
-    doc.text('Total de boletos vendidos: ' + totalBoletosVendidos, 30, y);
-    doc.text('Porcentaje de comision: ' + comision * 100, 30, y + 4);
-    doc.text('Venta: ' + totalVentas, 60, y + 10);
-    doc.text('Comision: ' + totalVentas * comision, 56, y + 14);
-    doc.text('Caja: ' + caja, 62.4, y + 18);
+    doc.text('Total de boletos vendidos: ' + totalBoletosVendidos, 20, y);
+    doc.text('Porcentaje de comision: ' + comision * 100, 20, y + 4);
+    doc.text('Venta: ' + totalVentas, 42, y + 10);
+    doc.text('Comision: ' + (totalVentas * comision).toFixed(2), 42, y + 14);
+    doc.text('Caja: ' + caja, 42.4, y + 18);
     doc.text('Adeudo pendiente vendedor: ' + deuda, 16, y + 30);
     doc.setFont("helvetica", "bold");
     doc.text('No incluido en el corte de caja', 16, y + 35);
