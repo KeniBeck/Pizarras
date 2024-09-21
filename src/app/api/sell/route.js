6fc991b5f7d7a10e47491bf3982eaca8e95ae963
Deyler Mena <deyler_mena69222@elpoli.edu.co>
2024-09-21 01:21:09 -0500

    correcion en corte de caja

--- src/app/api/sell/route.js
+++ src/app/api/sell/route.js
@@ -3,49 +3,95 @@ import pool from "@/db/MysqlConection";
 
 //normal
 export async function POST(req, res) {
-    let datos = await req.json();
-    const { fecha, idSorteo, idVendedor, name, primerPremio, prizebox, segundoPremio, ticketNumber, topePermitido } = datos;
+  let datos = await req.json();
+  const {
+    fecha,
+    idSorteo,
+    idVendedor,
+    name,
+    primerPremio,
+    prizebox,
+    segundoPremio,
+    ticketNumber,
+    topePermitido,
+  } = datos;
+  const fechaModificada = new Date(fecha)
+    .toISOString()
+    .slice(0, 19)
+    .replace("T", " ")
+    .split(" ")[0];
 
-    let sql = `
+  let sql = `
         INSERT INTO boletos
-        ( Fecha, Primerpremio, Segundopremio, Boleto, Costo, comprador, Idvendedor,tipo_sorteo)
-        VALUES( CURRENT_TIMESTAMP(), ?, ?, ?, ?, ?, ?, ?)
+        ( Fecha, Primerpremio, Segundopremio, Boleto, Costo, comprador, Idvendedor,tipo_sorteo,Fecha_venta)
+        VALUES( ?, ?, ?, ?, ?, ?, ?, ?,CURRENT_TIMESTAMP)
     `;
-    let sqlUpdate = `UPDATE topes SET  Cantidad = Cantidad + ${prizebox} WHERE Numero = ${ticketNumber}`;
-    let sqlSelect = `SELECT * FROM boletos WHERE Boleto = ? ORDER BY Idsorteo DESC LIMIT 1`;
-    let values = [primerPremio, segundoPremio, ticketNumber, prizebox, name, idVendedor, idSorteo];
+  let sqlUpdate = `UPDATE topes SET  Cantidad = Cantidad + ${prizebox} WHERE Numero = ${ticketNumber}`;
+  let sqlSelect = `SELECT * FROM boletos WHERE Boleto = ? ORDER BY Idsorteo DESC LIMIT 1`;
+  let values = [
+    fechaModificada,
+    primerPremio,
+    segundoPremio,
+    ticketNumber,
+    prizebox,
+    name,
+    idVendedor,
+    idSorteo,
+  ];
 
-    try {
-        let result = await pool.query(sql, values);
-        let resultUpdate = await pool.query(sqlUpdate);
-        let resultSelect = await pool.query(sqlSelect, [ticketNumber]);
+  try {
+    let result = await pool.query(sql, values);
+    let resultUpdate = await pool.query(sqlUpdate);
+    let resultSelect = await pool.query(sqlSelect, [ticketNumber]);
 
-        return NextResponse.json(resultSelect);
-    } catch (error) {
-        console.log(error);
-    }
+    return NextResponse.json(resultSelect);
+  } catch (error) {
+    console.log(error);
+  }
 }
 //serie
 export async function PUT(req, res) {
-    let datos = await req.json();
-    const { fecha, idSorteo, idVendedor, name, primerPremio, prizebox, segundoPremio, ticketNumber, topePermitido } = datos;
-
-
-    let sql = `
+  let datos = await req.json();
+  const {
+    fecha,
+    idSorteo,
+    idVendedor,
+    name,
+    primerPremio,
+    prizebox,
+    segundoPremio,
+    ticketNumber,
+    topePermitido,
+  } = datos;
+  const fechaModificada = new Date(fecha)
+    .toISOString()
+    .slice(0, 19)
+    .replace("T", " ")
+    .split(" ")[0];
+  let sql = `
         INSERT INTO boletos
-        ( Fecha, Primerpremio, Segundopremio, Boleto, Costo, comprador, Idvendedor,tipo_sorteo)
-        VALUES( CURRENT_TIMESTAMP(), ?, ?, ?, ?, ?, ?, ?)
+        ( Fecha, Primerpremio, Segundopremio, Boleto, Costo, comprador, Idvendedor,tipo_sorteo,Fecha_venta)
+        VALUES( ?, ?, ?, ?, ?, ?, ?, ?,CURRENT_TIMESTAMP)
     `;
-    // Obtener los últimos 10 elementos insertados
-    let sqlSelect = `SELECT * FROM boletos WHERE comprador = ? ORDER BY Idsorteo DESC LIMIT 10`;
+  // Obtener los últimos 10 elementos insertados
+  let sqlSelect = `SELECT * FROM boletos WHERE comprador = ? ORDER BY Idsorteo DESC LIMIT 10`;
 
-    let values = [primerPremio, segundoPremio, ticketNumber, prizebox, name, idVendedor, idSorteo];
+  let values = [
+    fechaModificada,
+    primerPremio,
+    segundoPremio,
+    ticketNumber,
+    prizebox,
+    name,
+    idVendedor,
+    idSorteo,
+  ];
 
-    try {
-        let result = await pool.query(sql, values);
-        let resultSelect = await pool.query(sqlSelect, [name]);
-        return NextResponse.json(resultSelect);
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    let result = await pool.query(sql, values);
+    let resultSelect = await pool.query(sqlSelect, [name]);
+    return NextResponse.json(resultSelect);
+  } catch (error) {
+    console.log(error);
+  }
 }
