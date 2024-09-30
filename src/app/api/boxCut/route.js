72c1145ae3c854be232b01d1e3f0d3d311c7a63b
Deyler Mena <deyler_mena69222@elpoli.edu.co>
2024-09-30 14:35:07 -0500

    cambiamos la hora alas 8pm

--- src/app/api/boxCut/route.js
+++ src/app/api/boxCut/route.js
@@ -17,43 +17,43 @@ export async function POST(req, res) {
 
   let startDate, endDate;
 
-  if (dayOfWeek === 0 && hourOfDay > 19) {
+  if (dayOfWeek === 0 && hourOfDay > 20) {
     // Domingo después de las 7pm
     startDate = new Date(now);
     startDate.setDate(now.getDate() - 2);
-    startDate.setHours(19, 0, 0, 0);
+    startDate.setHours(20, 0, 0, 0);
     endDate = new Date(now);
   } else if (dayOfWeek === 1 || (dayOfWeek === 2 && hourOfDay <= 19)) {
     // Lunes o Martes hasta las 7pm
     startDate = new Date(now);
     startDate.setDate(now.getDate() - (dayOfWeek + 1));
-    startDate.setHours(19, 0, 0, 0);
+    startDate.setHours(20, 0, 0, 0);
     endDate = new Date(now);
   } else if (
-    (dayOfWeek === 2 && hourOfDay > 19) ||
+    (dayOfWeek === 2 && hourOfDay > 20) ||
     dayOfWeek === 3 ||
     dayOfWeek === 4 ||
-    (dayOfWeek === 5 && hourOfDay <= 19)
+    (dayOfWeek === 5 && hourOfDay <= 20)
   ) {
     // Martes después de las 7pm hasta Viernes hasta las 7pm
     startDate = new Date(now);
     startDate.setDate(now.getDate() - (dayOfWeek - 2));
-    startDate.setHours(19, 0, 0, 0);
+    startDate.setHours(20, 0, 0, 0);
     endDate = new Date(now);
   } else if (
-    (dayOfWeek === 5 && hourOfDay > 19) ||
+    (dayOfWeek === 5 && hourOfDay > 20) ||
     dayOfWeek === 6 ||
-    (dayOfWeek === 0 && hourOfDay <= 19)
+    (dayOfWeek === 0 && hourOfDay <= 20)
   ) {
     // Viernes después de las 7pm hasta Domingo hasta las 7pm
     startDate = new Date(now);
     startDate.setDate(now.getDate() - (dayOfWeek - 5));
-    startDate.setHours(19, 0, 0, 0);
+    startDate.setHours(20, 0, 0, 0);
     endDate = new Date(now);
   }
 
-  startDate = startDate.toISOString().slice(0, 19).replace("T", " ");
-  endDate = endDate.toISOString().slice(0, 19).replace("T", " ");
+  startDate = startDate.toISOString().slice(0, 20).replace("T", " ");
+  endDate = endDate.toISOString().slice(0, 20).replace("T", " ");
 
   try {
     // Consulta para obtener los boletos especiales
--- src/app/api/model/ticketBuy.js
+++ src/app/api/model/ticketBuy.js
@@ -1,122 +1,123 @@
 import pool from "@/db/MysqlConection";
 export const selectLottery = async () => {
-    let result = false;
-    let error = false;
+  let result = false;
+  let error = false;
 
-    try {
-        const currentDate = new Date();
-        const dayOfWeek = currentDate.getDay(); // 0 (Sunday) to 6 (Saturday)
-        const currentHour = currentDate.getHours(); // Obtener la hora actual
-        let targetDate = new Date(currentDate);
+  try {
+    const currentDate = new Date();
+    const dayOfWeek = currentDate.getDay(); // 0 (Sunday) to 6 (Saturday)
+    const currentHour = currentDate.getHours(); // Obtener la hora actual
+    let targetDate = new Date(currentDate);
 
-        // Definir los días de los sorteos
-        const lotteryDays = {
-            2: 'Martes', // Martes
-            5: 'Viernes', // Viernes
-            0: 'Domingo' // Domingo
-        };
+    // Definir los días de los sorteos
+    const lotteryDays = {
+      2: "Martes", // Martes
+      5: "Viernes", // Viernes
+      0: "Domingo", // Domingo
+    };
 
-        // Función para obtener el siguiente día de sorteo
-        const getNextLotteryDay = (day) => {
-            if (day === 2) return 5; // Martes -> Viernes
-            if (day === 5) return 0; // Viernes -> Domingo
-            return 2; // Domingo -> Martes
-        };
+    // Función para obtener el siguiente día de sorteo
+    const getNextLotteryDay = (day) => {
+      if (day === 2) return 5; // Martes -> Viernes
+      if (day === 5) return 0; // Viernes -> Domingo
+      return 2; // Domingo -> Martes
+    };
 
-        // Si la hora actual es igual o mayor a las 19:00, obtener el siguiente día de sorteo
-        if (currentHour >= 19) {
-            targetDate.setDate(currentDate.getDate() + ((getNextLotteryDay(dayOfWeek) - dayOfWeek + 7) % 7));
-        } else {
-            // Obtener el próximo día de sorteo según el día actual
-            if (dayOfWeek === 1 || dayOfWeek === 2) { // Lunes o Martes
-                targetDate.setDate(currentDate.getDate() + (2 - dayOfWeek)); // Cargar el sorteo del Martes
-            } else if (dayOfWeek >= 3 && dayOfWeek <= 5) { // Miércoles, Jueves o Viernes
-                targetDate.setDate(currentDate.getDate() + (5 - dayOfWeek)); // Cargar el sorteo del Viernes
-            } else if (dayOfWeek === 6) { // Sábado
-                targetDate.setDate(currentDate.getDate() + 1); // Cargar el sorteo del Domingo
-            } else if (dayOfWeek === 0) { // Domingo
-                targetDate.setDate(currentDate.getDate() + 2); // Cargar el sorteo del Martes
-            }
-        }
-
-        const month = (targetDate.getMonth() + 1).toString().padStart(2, '0');
-        const day = targetDate.getDate().toString().padStart(2, '0');
-        const formattedDate = `${targetDate.getFullYear()}-${month}-${day}`;
-
-        let sql = `SELECT * FROM sorteo WHERE Fecha = ? AND (Tipo_sorteo = 'normal' OR (Tipo_sorteo = 'domingo' AND DAYOFWEEK(Fecha) = 1))`;
-        let [rows] = await pool.query(sql, [formattedDate]);
-        result = rows;
-
-    } catch (err) {
-        error = {
-            "sql": sql,
-            "description": err
-        }
-        console.log(error);
+    // Si la hora actual es igual o mayor a las 19:00, obtener el siguiente día de sorteo
+    if (currentHour >= 20) {
+      targetDate.setDate(
+        currentDate.getDate() +
+          ((getNextLotteryDay(dayOfWeek) - dayOfWeek + 7) % 7)
+      );
+    } else {
+      // Obtener el próximo día de sorteo según el día actual
+      if (dayOfWeek === 1 || dayOfWeek === 2) {
+        // Lunes o Martes
+        targetDate.setDate(currentDate.getDate() + (2 - dayOfWeek)); // Cargar el sorteo del Martes
+      } else if (dayOfWeek >= 3 && dayOfWeek <= 5) {
+        // Miércoles, Jueves o Viernes
+        targetDate.setDate(currentDate.getDate() + (5 - dayOfWeek)); // Cargar el sorteo del Viernes
+      } else if (dayOfWeek === 6) {
+        // Sábado
+        targetDate.setDate(currentDate.getDate() + 1); // Cargar el sorteo del Domingo
+      } else if (dayOfWeek === 0) {
+        // Domingo
+        targetDate.setDate(currentDate.getDate() + 2); // Cargar el sorteo del Martes
+      }
     }
 
-    let response = {
-        "preocess": 'select table',
-        "status": true,
-        "result": result
-    }
+    const month = (targetDate.getMonth() + 1).toString().padStart(2, "0");
+    const day = targetDate.getDate().toString().padStart(2, "0");
+    const formattedDate = `${targetDate.getFullYear()}-${month}-${day}`;
 
-    return response;
-}
+    let sql = `SELECT * FROM sorteo WHERE Fecha = ? AND (Tipo_sorteo = 'normal' OR (Tipo_sorteo = 'domingo' AND DAYOFWEEK(Fecha) = 1))`;
+    let [rows] = await pool.query(sql, [formattedDate]);
+    result = rows;
+  } catch (err) {
+    error = {
+      sql: sql,
+      description: err,
+    };
+    console.log(error);
+  }
+
+  let response = {
+    preocess: "select table",
+    status: true,
+    result: result,
+  };
+
+  return response;
+};
 
 export const selectLotterySerie = async () => {
-    let result = false;
-    let error = false;
+  let result = false;
+  let error = false;
 
-    try {
-        let sql = `SELECT s.Fecha AS 'fecha_sorteo', b.*
+  try {
+    let sql = `SELECT s.Fecha AS 'fecha_sorteo', b.*
                         FROM sorteo s
                         JOIN boletos b ON s.Idsorteo  = b.tipo_sorteo
-                        WHERE s.Tipo_sorteo = 'especial' AND s.Estatus = 'abierto';`
-        let [rows] = await pool.query(sql);
-        result = rows;
+                        WHERE s.Tipo_sorteo = 'especial' AND s.Estatus = 'abierto';`;
+    let [rows] = await pool.query(sql);
+    result = rows;
+  } catch (err) {
+    error = {
+      sql: sql,
+      description: err,
+    };
+    console.log(error);
+  }
 
+  let response = {
+    preocess: "select table",
+    status: true,
+    result: result,
+  };
 
-
-    } catch (err) {
-        error = {
-            "sql": sql,
-            "description": err
-        }
-        console.log(error);
-    }
-
-    let response = {
-        "preocess": 'select table',
-        "status": true,
-        "result": result
-    }
-
-    return response;
-}
+  return response;
+};
 export const selectLotteryEspecial = async () => {
-    let result = false;
-    let error = false;
+  let result = false;
+  let error = false;
 
-    try {
-        let sql = `SELECT * FROM sorteo WHERE Tipo_sorteo ='especial' AND Estatus = 'abierto'`;
-        let [rows] = await pool.query(sql);
-        result = rows;
+  try {
+    let sql = `SELECT * FROM sorteo WHERE Tipo_sorteo ='especial' AND Estatus = 'abierto'`;
+    let [rows] = await pool.query(sql);
+    result = rows;
+  } catch (err) {
+    error = {
+      sql: sql,
+      description: err,
+    };
+    console.log(error);
+  }
 
+  let response = {
+    preocess: "select table",
+    status: true,
+    result: result,
+  };
 
-    } catch (err) {
-        error = {
-            "sql": sql,
-            "description": err
-        }
-        console.log(error);
-    }
-
-    let response = {
-        "preocess": 'select table',
-        "status": true,
-        "result": result
-    }
-
-    return response;
-}
+  return response;
+};
