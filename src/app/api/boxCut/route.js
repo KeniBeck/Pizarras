f99c090c10fc4ba6ab328c1e6767b091e8fc8f8d
Deyler Mena <deyler_mena69222@elpoli.edu.co>
2024-10-02 03:10:18 -0500

    se cambio la logica del corte de caja

--- src/app/api/boxCut/route.js
+++ src/app/api/boxCut/route.js
@@ -3,57 +3,13 @@ import { NextResponse } from "next/server";
 
 export async function POST(req, res) {
   const data = await req.json();
-  const { Idvendedor, Fechaingreso } = data;
+  const { Idvendedor, Fechaingreso, sucursal } = data;
 
   const fecha = new Date(Fechaingreso)
     .toISOString()
     .slice(0, 19)
     .replace("T", " ")
     .split(" ")[0];
-
-  const now = new Date();
-  const dayOfWeek = now.getDay();
-  const hourOfDay = now.getHours();
-
-  let startDate, endDate;
-
-  if (dayOfWeek === 0 && hourOfDay > 20) {
-    // Domingo después de las 7pm
-    startDate = new Date(now);
-    startDate.setDate(now.getDate() - 2);
-    startDate.setHours(20, 0, 0, 0);
-    endDate = new Date(now);
-  } else if (dayOfWeek === 1 || (dayOfWeek === 2 && hourOfDay <= 19)) {
-    // Lunes o Martes hasta las 7pm
-    startDate = new Date(now);
-    startDate.setDate(now.getDate() - (dayOfWeek + 1));
-    startDate.setHours(20, 0, 0, 0);
-    endDate = new Date(now);
-  } else if (
-    (dayOfWeek === 2 && hourOfDay > 20) ||
-    dayOfWeek === 3 ||
-    dayOfWeek === 4 ||
-    (dayOfWeek === 5 && hourOfDay <= 20)
-  ) {
-    // Martes después de las 7pm hasta Viernes hasta las 7pm
-    startDate = new Date(now);
-    startDate.setDate(now.getDate() - (dayOfWeek - 2));
-    startDate.setHours(20, 0, 0, 0);
-    endDate = new Date(now);
-  } else if (
-    (dayOfWeek === 5 && hourOfDay > 20) ||
-    dayOfWeek === 6 ||
-    (dayOfWeek === 0 && hourOfDay <= 20)
-  ) {
-    // Viernes después de las 7pm hasta Domingo hasta las 7pm
-    startDate = new Date(now);
-    startDate.setDate(now.getDate() - (dayOfWeek - 5));
-    startDate.setHours(20, 0, 0, 0);
-    endDate = new Date(now);
-  }
-
-  startDate = startDate.toISOString().slice(0, 20).replace("T", " ");
-  endDate = endDate.toISOString().slice(0, 20).replace("T", " ");
 
   try {
     // Consulta para obtener los boletos especiales
@@ -63,7 +19,7 @@ export async function POST(req, res) {
         JOIN sorteo s ON b.tipo_sorteo = s.Idsorteo
         JOIN vendedores v ON b.Idvendedor = v.Idvendedor
         LEFT JOIN deuda d ON v.Idvendedor = d.usuario
-        WHERE s.Tipo_sorteo = 'especial' AND b.Fecha_venta BETWEEN ? AND ?
+        WHERE s.Tipo_sorteo = 'especial' AND DATE(b.Fecha) = DATE(s.Fecha) AND corte_caja = FALSE;
         `;
 
     // Consulta para obtener los boletos normales
@@ -73,22 +29,85 @@ export async function POST(req, res) {
         JOIN sorteo s ON b.tipo_sorteo = s.Idsorteo
         JOIN vendedores v ON b.Idvendedor = v.Idvendedor
         LEFT JOIN deuda d ON v.Idvendedor = d.usuario
-        WHERE s.Tipo_sorteo = 'normal' AND b.Fecha_venta BETWEEN ? AND ?;
+        WHERE s.Tipo_sorteo = 'normal' AND DATE(b.Fecha) = DATE(s.Fecha) AND corte_caja = FALSE;
         `;
 
     // Ejecutar las consultas
-    let boletosEspeciales = await pool.query(sqlEspeciales, [
-      startDate,
-      endDate,
-    ]);
-    let boletosNormales = await pool.query(sqlNormales, [startDate, endDate]);
+    let [boletosEspeciales] = await pool.query(sqlEspeciales);
+    let [boletosNormales] = await pool.query(sqlNormales);
+
+    // Combinar los boletos en un solo array
+    let boletos = [...boletosEspeciales, ...boletosNormales];
+
+    // Obtener los IDs de los boletos seleccionados
+    let ids = boletos.map((boleto) => boleto.Idsorteo);
+
+    // Actualizar el atributo corte_caja a true para los boletos seleccionados
+    if (ids.length > 0) {
+      let sqlUpdate = `
+          UPDATE boletos
+          SET corte_caja = TRUE
+          WHERE Idsorteo IN (${ids.join(",")});
+          `;
+      await pool.query(sqlUpdate);
+    }
+
+    // Calcular los valores necesarios
+    let totalBoletosVendidos = boletos.length;
+    let totalVentas = boletos.reduce(
+      (total, boleto) => total + boleto.Costo,
+      0
+    );
+    let comision = boletos.length > 0 ? boletos[0].comisiones / 100 : 0;
+    let deuda = boletos.length > 0 ? boletos[0].deuda : 0;
+    let caja = totalVentas - totalVentas * comision;
+    let totalEntregado = totalVentas - totalVentas * 0.1;
+
+    // Determinar el tipo de sorteo
+    let tipoSorteo = "";
+    if (boletosEspeciales.length > 0 && boletosNormales.length > 0) {
+      tipoSorteo = "especial y normal";
+    } else if (boletosEspeciales.length > 0) {
+      tipoSorteo = "especial";
+    } else if (boletosNormales.length > 0) {
+      tipoSorteo = "normal";
+    }
+
+    // Verificar y ajustar los valores antes de la inserción
+    if (totalBoletosVendidos === 0) {
+      return NextResponse.json({
+        message: "No se insertó porque los valores están en 0",
+      });
+    }
+
+    // Insertar la información en la tabla cortesdecaja
+    let sqlInsert = `
+        INSERT INTO cortesdecaja
+        (idcorte, Fecha_actual, fechasorteo, vendedor, sucursal, boletosvendidos, venta, porcentajecomision, comision, totalcaja, Tipo_sorteo, totalentregado)
+        VALUES (0, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+        `;
+    let values = [
+      new Date().toISOString().slice(0, 19).replace("T", " "),
+      fecha,
+      Idvendedor,
+      sucursal,
+      totalBoletosVendidos,
+      totalVentas,
+      comision * 100,
+      totalVentas * comision,
+      caja,
+      tipoSorteo,
+      totalEntregado,
+    ];
+    await pool.query(sqlInsert, values);
 
     // Devolver los resultados
     return NextResponse.json({
-      boletosEspeciales: boletosEspeciales[0],
-      boletosNormales: boletosNormales[0],
+      boletosEspeciales: boletosEspeciales,
+      boletosNormales: boletosNormales,
     });
   } catch (error) {
     console.log(error);
+    return NextResponse.json({ error: "Error al ejecutar la consulta" });
   }
 }
