b54def63acbe503d81a869a86193e1b2b5fad6bf
Deyler Mena <deyler_mena69222@elpoli.edu.co>
2024-10-23 19:36:47 -0500

    actulizacion de puntos y tope

---
+++ src/components/custom/validation/updateInfo.js
@@ -0,0 +1,23 @@
+const updateInfo = async (userId) => {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ userId })
+    };
+
+    try {
+        const response = await fetch("/api/user", options);
+        const data = await response.json();
+
+        // Actualizar la información en localStorage
+        localStorage.setItem('userData', JSON.stringify(data));
+
+        console.log('Data actualizada en localStorage:', data);
+    } catch (error) {
+        console.error('Error al actualizar la información:', error);
+    }
+};
+
+export default updateInfo;
