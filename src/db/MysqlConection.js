f8ca0f003283394fda4fef52101b79d126bfeb75
Deyler Mena <deyler_mena69222@elpoli.edu.co>
2024-11-12 16:45:11 -0500

    cambiamos los datos de conexion ala db

--- src/db/MysqlConection.js
+++ src/db/MysqlConection.js
@@ -1,12 +1,15 @@
 import mysql2 from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const pool = mysql2.createPool({
-    host: "162.240.174.79",
-    user: "pizarras_andres",
-    database: "pizarras_pizarritas",
-    password: "r@@tPizaras",
-    port: "3306",
-    timezone: "Z"
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  timezone: process.env.DB_TIMEZONE,
+});
 
 export default pool;
