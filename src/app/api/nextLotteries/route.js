4c7e2c58990a6f04b3f306dafa500806149bb5b1
CristianDev <150852071+CristianDB1@users.noreply.github.com>
2025-11-21 16:42:10 -0500

    fix api nextlotteries
    Ahora se agrega una verificacion al tipo de sorteo para saber si es especial o no y se cargue correctamente

--- src/app/api/nextLotteries/route.js
+++ src/app/api/nextLotteries/route.js
@@ -3,16 +3,18 @@ export const dynamic = "force-dynamic";
 import { NextResponse } from "next/server";
 import pool from "@/db/MysqlConection";
 
-// Devuelve los próximos 3 sorteos activos (sin limitar por tipo de día)
+// Devuelve los próximos 3 sorteos activos normales
 export async function GET(req) {
   try {
-    // Traer los próximos 3 sorteos abiertos, sin filtrar por tipo de sorteo
+    // Traer los próximos 3 sorteos abiertos que no sean especiales
     const [rows] = await pool.query(`
       SELECT * FROM sorteo
       WHERE Estatus = 'abierto'
+      AND Tipo_sorteo != 'especial'  
       ORDER BY Fecha ASC
       LIMIT 3
     `);
+    
     const res = NextResponse.json({ result: rows });
     res.headers.set('Cache-Control', 'no-store');
     return res;
